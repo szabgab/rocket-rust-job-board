c9bbc5e28a65e99a5d1244850c46678473591fae
Gabor Szabo <gabor@szabgab.com>
2024-08-06 18:40:13 +0300

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", context! {
-        name: "Rocket with Tera"
-    })
+    Template::render(
+        "index",
+        context! {
+            name: "Rocket with Tera"
+        },
+    )
 }
 
 #[get("/add")]
 fn add() -> Template {
-    Template::render("add", context! {
-        title: "Add a Rust job"
-    })
+    Template::render(
+        "add",
+        context! {
+            title: "Add a Rust job"
+        },
+    )
 }
-
 
 #[launch]
 fn rocket() -> _ {
