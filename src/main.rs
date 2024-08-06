84a7a0650a2399c62f7efe0bced915407b9df225
Gabor Szabo <gabor@szabgab.com>
2024-08-06 18:16:28 +0300

    switch to tera templates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket_dyn_templates::{context, Template};
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {
+        name: "Rocket with Tera"
+    })
 }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+}
+
+#[cfg(test)]
+mod tests;
